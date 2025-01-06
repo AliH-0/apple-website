384b8f2daa799158421dea072fbf557af1be3d6e
AliH-0 <ali.h.husseini0@gmail.com>
2025-01-06 06:08:25 +0200

    Added features.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,13 @@ import * as Sentry from "@sentry/react";
 //initializing sentry
 Sentry.init({
   dsn: "https://aee621ddef8cc670645044291eba735a@o4508588757286912.ingest.de.sentry.io/4508593087709265",
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+  ],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   replaysSessionSampleRate: 0.1,
